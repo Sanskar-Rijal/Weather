0f7535b2d3fc83ad33ba1d3ea4d7fd5974aeba99
Sanskar-Rijal <rijalsanskar@gmail.com>
2024-10-14 17:07:36 +0545

    done with splash screen

--- app/src/main/java/com/example/weather/navigation/WeatherNavigation.kt
+++ app/src/main/java/com/example/weather/navigation/WeatherNavigation.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavHost
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.weather.Screens.MainScreen
 import com.example.weather.Screens.WeatherSplashScreen
 
 //is the navcontroller , has the ability to control any screen in the app
@@ -18,7 +19,11 @@ fun WeatherNavigation() {
        startDestination = WeatherScreens.SplashScreen.name){
        //creating navgraphs
        composable(WeatherScreens.SplashScreen.name){
-           WeatherSplashScreen(navController =navController)
+           WeatherSplashScreen(navController)
+       }
+
+       composable(WeatherScreens.MainScreen.name){
+           MainScreen(navController)
        }
    }
 }
